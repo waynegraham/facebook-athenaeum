fe96920c6081a1a5a164c906a0bfe54e161e755a
wayne.graham <wayne.graham@9d3f1507-a950-0410-b181-95b310c3f84d>
2008-07-11 17:48:40 +0000

    Fixed errant characters in the MySQL script

--- trunk/mysql_helper.sh
+++ trunk/mysql_helper.sh
@@ -1,42 +1,48 @@
-#! /bin/bash
+#!/bin/bash
 
 #
 # Helper script to set up MySQL datasource
 # $Id$
 #
 
-## Begin
+## Begin Script
 clear
-echo "Welcome to the MySQL Setup Script"
-echo "This will set up the MySQL database for your application"
+echo "Welcome to the MySQL Setup Script."
+echo "This will setup the MySQL Database"
 echo ""
 
-read -p "Database Name [fbathenaeum]: " MYSQLDB
-read -p "Database User [fbuser]: " MYSQLNEWUSER
-read -p "User Password: " -s MYSQLNEWPASS
-
+## Prompt for Database values
+read -p "New Database Name [fbathenaeum]: " MYSQLDB
+read -p "New Database User [fbuser]: " MYSQLNEWUSER
+read -p "New User Password: " -s MYSQLNEWPASS
 echo ""
 read -p "MySQL Host [localhost]: " MYSQLHOST
 read -p "MySQL Root User [root]: " MYSQLUSER
 read -p "MySQL Root Password: " -s MYSQLPASS
 echo ""
 
-## Set defaults
+
+## Set Defaults if selected
 if [ -z $MYSQLDB ]; then
-	MYSQLDB=fbathenaeum
+     MYSQLDB=fbathenaeum
 fi
 if [ -z $MYSQLNEWUSER ]; then
-	MYSQLNEWUSER=fbuser
+    MYSQLNEWUSER=fbuser
 fi
 if [ -z $MYSQLHOST ]; then
-	MYSQLHOST=localhost
+    MYSQLHOST=localhost
 fi
 if [ -z $MYSQLUSER ]; then
-	MYSQLUSER=root
+    MYSQLUSER=root
 fi
 
-## Process database
+
+## Process creating mysql user and database
 mysqladmin -h $MYSQLHOST -u $MYSQLUSER -p$MYSQLPASS create $MYSQLDB
 mysql -h $MYSQLHOST -u $MYSQLUSER -p$MYSQLPASS -e "GRANT SELECT,INSERT,UPDATE,DELETE ON $MYSQLDB.* TO '$MYSQLNEWUSER'@'$MYSQLHOST' IDENTIFIED BY '$MYSQLNEWPASS' WITH GRANT OPTION"
 mysql -h $MYSQLHOST -u $MYSQLUSER -p$MYSQLPASS -e "FLUSH PRIVILEGES"
 mysql -h $MYSQLHOST -u $MYSQLUSER -p$MYSQLPASS -D $MYSQLDB < libs/mysql.sql
+
+
+echo "MySQL Setup is now Complete"
+
