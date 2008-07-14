30a78caecfb120456a7e27ba340a86202a654a36
phillip.fenstermacher <phillip.fenstermacher@9d3f1507-a950-0410-b181-95b310c3f84d>
2008-07-14 17:48:15 +0000

    Fixed MySQL script.

--- trunk/libs/mysql.sql
+++ trunk/libs/mysql.sql
@@ -3,9 +3,9 @@ $Id$
 */
 CREATE TABLE `locations`(
 	`uid` bigint(20) NOT NULL,
-	`x` int(11) NOT NULL,
-	`y` int(11) NOT NULL,
-	`floor` int(11) NOT NULL,
+	`x` int(11),
+	`y` int(11),
+	`floor` int(11),
 	`updated` timestamp NOT NULL default CURRENT_TIMESTAMP,
 	PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
