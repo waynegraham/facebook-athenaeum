dd41a483e34130693fff41739c3868bb7c99edd8
wayne.graham <wayne.graham@9d3f1507-a950-0410-b181-95b310c3f84d>
2008-11-11 21:12:09 +0000

    Updated to latest Smarty package

--- trunk/smarty_helper.sh
+++ trunk/smarty_helper.sh
@@ -17,12 +17,12 @@ PHPDIR=`pear config-get php_dir`
 SMARTYDIR="$PHPDIR/Smarty"
 if [ ! -d $SMARTYDIR ]
 then
-    wget http://smarty.php.net/do_download.php?download_file=Smarty-2.6.19.tar.gz
-    tar -zxf Smarty-2.6.19.tar.gz
+    wget http://www.smarty.net/do_download.php?download_file=Smarty-2.6.20.tar.gz
+    tar -zxf Smarty-2.6.20.tar.gz
     mkdir $SMARTYDIR
-    mv Smarty-2.6.19/libs/* $SMARTYDIR
-    rm Smarty-2.6.19.tar.gz
-    rm -rf Smarty-2.6.19
+    mv Smarty-2.6.20/libs/* $SMARTYDIR
+    rm Smarty-2.6.20.tar.gz
+    rm -rf Smarty-2.6.20
 fi
 
 ## Install PEAR packages
